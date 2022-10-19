d612ac18e19082f860b1d4f7dff69b20a9ad1773
PavelPonomarchuk <ponomarchuk83@gmail.com>
2022-10-19 13:05:37 +0500

    Add possibility of removing data from history

--- app/src/main/java/ru/ponomarchukpn/pavelweatherapp/ShowHistoryActivity.java
+++ app/src/main/java/ru/ponomarchukpn/pavelweatherapp/ShowHistoryActivity.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -18,6 +20,7 @@ public class ShowHistoryActivity extends AppCompatActivity {
     private RecyclerView recyclerViewHistory;
     private Button btnGoToMain;
     private WeatherDataDatabase weatherDataDatabase;
+    private WeatherDataAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +31,7 @@ public class ShowHistoryActivity extends AppCompatActivity {
         weatherDataDatabase = WeatherDataDatabase.getInstance(this);
         getData();
 
-        WeatherDataAdapter adapter = new WeatherDataAdapter(weatherDataList);
+        adapter = new WeatherDataAdapter(weatherDataList);
         recyclerViewHistory.setLayoutManager(new LinearLayoutManager(this));
         recyclerViewHistory.setAdapter(adapter);
 
@@ -36,11 +39,31 @@ public class ShowHistoryActivity extends AppCompatActivity {
             Intent intent = new Intent(ShowHistoryActivity.this, MainActivity.class);
             startActivity(intent);
         });
+
+        ItemTouchHelper helper = new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+                removeFromRecyclerView(viewHolder.getAdapterPosition());
+            }
+        });
+        helper.attachToRecyclerView(recyclerViewHistory);
+    }
+
+    private void removeFromRecyclerView(int position) {
+        WeatherData weatherData = weatherDataList.get(position);
+        weatherDataDatabase.weatherDataDao().deleteWeatherData(weatherData);
+        getData();
+        adapter.notifyDataSetChanged();
     }
 
     private void getData() {
-        List<WeatherData> datafromDB = weatherDataDatabase.weatherDataDao().detAllWeatherData();
+        List<WeatherData> dataFromDB = weatherDataDatabase.weatherDataDao().detAllWeatherData();
         weatherDataList.clear();
-        weatherDataList.addAll(datafromDB);
+        weatherDataList.addAll(dataFromDB);
     }
 }
